09fdb3e7782643a99c1fe3dccdb68c71376c6bd8
PoTuStoronu222 <maximxaker1@gmail.com>
2026-09-03 15:49:57 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ MANAGER_PATH="/usr/bin/dns-manager"
 # ==========================================
 # ОСНОВНЫЕ ПАРАМЕТРЫ
 # ==========================================
-VERSION="1.13-HYBRID"
+VERSION="1.14-HYBRID"
 BASE_DIR="/etc/dns-manager"
 CFG_DIR="$BASE_DIR/config"
 STATE_DIR="/var/run/dns-manager"
